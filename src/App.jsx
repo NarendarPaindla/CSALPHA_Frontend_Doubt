bba22afffc5307d538c360c5ea8479744c0133f3
narendarpaindla <narendarreddypaindla@gmail.com>
2025-03-22 06:58:28 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import ForgotPassword from './components/auth/ForgotPassword';
+import ResetPassword from './components/auth/ResetPassword';
 
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
     </Routes>
   );
 }
--- src/services/authService.js
+++ src/services/authService.js
@@ -4,9 +4,20 @@ const API_URL="https://c1s4i0-8080.bytexl.dev/api/auth";
 const register =async(user)=>{
     const response=await axios.post(`${API_URL}/register`,user);
     return response.data;
-}
+} 
 const login = async (email, password) => {
     const response = await axios.post(`${API_URL}/login`, { email, password });
     return response.data;
   };
-export default {register,login};
+  const forgotPassword = async (email) => {
+    const response = await axios.post(`${API_URL}/forgot-password`, { email });
+    return response.data;
+  };
+  
+  const resetPassword = async (token, newPassword) => {
+    const response = await axios.post(`${API_URL}/reset-password`, { token, newPassword });
+    return response.data;
+  };
+export default {register,login,
+  forgotPassword,
+  resetPassword};
